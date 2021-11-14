7ea812a4156f383a06f012d7da8487237c3e7183
Hhanuska <41262567+Hhanuska@users.noreply.github.com>
2021-11-14 13:37:59 +0100

    Navbar css

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,36 @@
 import { NextPage } from "next";
 import Link from 'next/link';
+import styles from '../styles/navbar.module.css'
+import { useRouter } from 'next/router'
 
 const NavBar: NextPage = () => {
+    const router = useRouter();
+
+    const navigation = [
+        { href: '/', text: 'Home' },
+        { href: '/downloader', text: 'Downloader' },
+        { href: '/backtest', text: 'Backtest' }
+    ]
 
     return (
-        <table>
-            <tr>
-                <th>
-                    <Link href='/'>Home</Link>
-                </th>
-                <th>
-                    <Link href='/downloader'>Downloader</Link>
-                </th>
-                <th>
-                    <Link href='/backtest'>Backtest</Link>
-                </th>
-            </tr>
-        </table>
+        <div className={styles.navbar}>
+            <ul>
+                {
+                    navigation.map((e) => (
+                        <li key={e.text}>
+                            <Link href={e.href}>
+                                <a className={isActiveClass(router.route, e.href)}>{e.text}</a>
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
     )
+}
+
+const isActiveClass = (router: string, href: string): string => {
+    return router === href ? 'active' : '';
 }
 
 export default NavBar;
